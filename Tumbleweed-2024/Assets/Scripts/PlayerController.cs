2b9f309ff69d7cecf8be9478478c9328eff6d66b
Remmiemmyjr <emily@isimar.com>
2024-01-26 21:10:09 -0800

    Basic acceleration controller

--- Tumbleweed-2024/Assets/Scripts/PlayerController.cs
+++ Tumbleweed-2024/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+    Vector2 dir;
+    float accelerationRate = 10.0f;
+    float changeDirForce = 10.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovePlayer();
     }
+
+    public void OnMovementInput(InputAction.CallbackContext ctx)
+    {
+        dir = ctx.ReadValue<Vector2>();
+    }
+
+    void MovePlayer()
+    {
+        dir = dir.normalized;
+        Vector3 moveVec = new Vector3(dir.x, rb.velocity.y, dir.y);
+
+        //float dotProduct = Vector3.Dot(rb.velocity.normalized, dir);
+
+        rb.AddForce(moveVec * accelerationRate);
+    }    
 }
