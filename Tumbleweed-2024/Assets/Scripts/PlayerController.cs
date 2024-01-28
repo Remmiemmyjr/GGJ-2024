307ed220749dc2865f4e6cf476c875f097b7fed8
Remmiemmyjr <emily@isimar.com>
2024-01-27 16:17:12 -0800

    Made bullet counter for abilities

--- Tumbleweed-2024/Assets/Scripts/PlayerController.cs
+++ Tumbleweed-2024/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 currMoveVec;
+    Coroutine? reloadCo;
+
     public Transform Camera;
 
-    public int maxBullets;
+    public int maxBullets = 6;
     [HideInInspector]
     public int bullets;
 
     float dirVal;
     float speed;
+
+    public float reloadTime = 2.0f;
 
     [SerializeField]
     float accelerationRate = 30.0f;
@@ -28,9 +33,12 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     float jumpBoostForce = 3.0f;
+    float jumpCoolDown = 1f;
+    float lastJumpTime = -1f;
 
-    bool isBoosting;
-    bool canBoost;
+    bool isSpeedBoosting;
+    bool canSpeedBoost;
+    bool canReload;
 
     // Start is called before the first frame update
     void Start()
@@ -38,7 +46,9 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         speed = accelerationRate;
         currTime = speedBoostTime;
-        canBoost = true;
+        canSpeedBoost = true;
+        bullets = maxBullets;
+        canReload = true;
     }
 
     // Update is called once per frame
@@ -50,24 +60,24 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (isBoosting)
+        if (isSpeedBoosting)
         {
-            canBoost = false;
             if (currTime > 0)
             {
                 currTime -= Time.deltaTime;
             }
             else if (currTime <= 0)
             {
-                isBoosting = false;
-                canBoost = true;
+                isSpeedBoosting = false;
+                canSpeedBoost = true;
                 currTime = speedBoostTime;
             }
         }
 
-        if (bullets <= 0)
+        if (bullets <= 0 && canReload)
         {
-
+            canReload = false;
+            StartCoroutine(ReloadGun());
         }
     }
 
@@ -78,35 +88,59 @@ public class PlayerController : MonoBehaviour
 
     public void OnSpeedBoostInput(InputAction.CallbackContext ctx)
     {
-        if (canBoost)
+        if(bullets <= 0) return;
+        if (Time.realtimeSinceStartup - lastJumpTime < jumpCoolDown) return;
+
+
+        if (currMoveVec != Vector3.zero && canSpeedBoost && bullets > 0)
         {
-            isBoosting = true;
+            canSpeedBoost = false;
+            isSpeedBoosting = true;
             rb.velocity = new Vector3(rb.velocity.x * speedBoost, rb.velocity.y, rb.velocity.z * speedBoost);
-            bullets--;
+
+            lastJumpTime = Time.realtimeSinceStartup;
+            
+            // BULLETS
+            bullets -= 1;
+            Debug.Log(bullets);
         }
     }
 
     public void OnJumpBoostInput(InputAction.CallbackContext ctx)
     {
-        if (ctx.canceled) return;
-        if (ctx.performed)
+        if (ctx.canceled || bullets <= 0) return;
+
+        if (ctx.performed && bullets > 0)
         {
             Vector3 jumpVec = new Vector3(rb.velocity.x, jumpBoostForce, rb.velocity.z);
             rb.velocity = jumpVec;
-            bullets--;
+
+            // BULLETS
+            bullets -= 1;
+            Debug.Log(bullets);
         }
     }
 
     void MovePlayer()
     {
-        Vector3 currMoveVec = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        currMoveVec = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
         rb.AddForce((Camera.forward * dirVal) * speed);
-        if(currMoveVec.magnitude > maxSpeed && !isBoosting)
+        if(currMoveVec.magnitude > maxSpeed && !isSpeedBoosting)
         {
             currMoveVec = currMoveVec.normalized * maxSpeed;
             Vector3 newVel = new Vector3(currMoveVec.x, rb.velocity.y, currMoveVec.z);
             rb.velocity = newVel;
         }
+    }
+
+    IEnumerator ReloadGun()
+    {
+        //Animation
+        yield return new WaitForSeconds(reloadTime);
+        bullets = maxBullets;
+        Debug.Log("Reloaded");
+        Debug.Log(bullets);
+        canReload = true;
     }
 }
