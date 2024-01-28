2924b63a839162d4eb50379f0c37face171e5d29
Corbyn LaMar <46691597+corbyn-lamar@users.noreply.github.com>
2024-01-28 15:08:28 -0800

    MenuCanvas Setting Serialized

--- Tumbleweed-2024/Assets/Scripts/MainMenuManager.cs
+++ Tumbleweed-2024/Assets/Scripts/MainMenuManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
@@ -18,8 +20,46 @@ public class MainMenuManager : MonoBehaviour
     private float cameraTargetRotation = 0.0f;
     private CamRotationState camRotationState = CamRotationState.BOARD_MAIN;
 
+    // Settings Menu
+    [SerializeField]
+    private Slider settingsMusicVolSlider;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI settingsMusicVolText;
+    [SerializeField]
+    private Slider settingsSFXVolSlider;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI settingsSFXVolText;
+    [SerializeField]
+    private Slider settingsAmbianceVolSlider;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI settingsAmbianceVolText;
+    [SerializeField]
+    private Slider settingsCameraSensSlider;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI settingsCameraSensText;
+
     [HideInInspector]
     public bool inSubMenu = false;
+
+    private void Start()
+    {
+        float musicVol = PlayerPrefs.GetFloat("MusicVol");
+        settingsMusicVolText.text = "" + musicVol;
+        settingsMusicVolSlider.value = musicVol;
+
+        float sfxVol = PlayerPrefs.GetFloat("SFXVol");
+        settingsSFXVolText.text = "" + sfxVol;
+        settingsSFXVolSlider.value = sfxVol;
+
+        float ambianceVol = PlayerPrefs.GetFloat("AmbianceVol");
+        settingsAmbianceVolText.text = "" + ambianceVol;
+        settingsAmbianceVolSlider.value = ambianceVol;
+
+        float cameraSense = PlayerPrefs.GetFloat("CameraSens");
+        settingsCameraSensText.text = "" + cameraSense;
+        settingsCameraSensSlider.value = cameraSense;
+
+    }
 
     private void Update()
     {
@@ -49,6 +89,31 @@ public class MainMenuManager : MonoBehaviour
     public void ReturnButton()
     {
         cameraTargetRotation = (float)CamRotationState.BOARD_MAIN;
+    }
+
+    public void SettingsMusicSlider()
+    {
+        settingsMusicVolText.text = "" + (int)(settingsMusicVolSlider.value * 100.0f);
+        PlayerPrefs.SetFloat("MusicVol", settingsMusicVolSlider.value);
+    }
+
+    public void SettingsSFXSlider()
+    {
+        settingsSFXVolText.text = "" + (int)(settingsSFXVolSlider.value * 100.0f);
+        PlayerPrefs.SetFloat("SFXVol", settingsSFXVolSlider.value);
+    }
+
+    public void SettingsAmbianceSlider()
+    {
+        settingsAmbianceVolText.text = "" + (int)(settingsAmbianceVolSlider.value * 100.0f);
+        PlayerPrefs.SetFloat("AmbianceVol", settingsAmbianceVolSlider.value);
+    }
+
+    public void SettingsCameraSensSlider()
+    {
+        settingsCameraSensText.text = "" + (settingsCameraSensSlider.value).ToString("F2")
+;
+        PlayerPrefs.SetFloat("CameraSens", settingsCameraSensSlider.value);
     }
 
     public void ReturnMenu()
