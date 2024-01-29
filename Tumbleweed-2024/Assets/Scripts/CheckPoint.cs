246fa66c0d231b533d053d96a9a6a3dda2e4fae4
Corbyn LaMar <46691597+corbyn-lamar@users.noreply.github.com>
2024-01-28 16:52:56 -0800

    Confetti Add

--- Tumbleweed-2024/Assets/Scripts/CheckPoint.cs
+++ Tumbleweed-2024/Assets/Scripts/CheckPoint.cs
@@ -7,6 +7,8 @@ public class CheckPoint : MonoBehaviour
     public AudioSource checkpoint;
     public AudioClip[] checkclips;
 
+    public ParticleSystem ps;
+
     bool checkPointReached = false;
 
     // Storage Variables
@@ -31,5 +33,7 @@ public class CheckPoint : MonoBehaviour
         // Set the position and time attributes within the player controller
         other.GetComponent<PlayerController>().resetPosition = checkPointPosition - new Vector3(0.0f, (transform.localScale.y / 4.0f), 0.0f);
         other.GetComponent<PlayerController>().timeOnReset = time;
+
+        ps.Play();
     }
 }
